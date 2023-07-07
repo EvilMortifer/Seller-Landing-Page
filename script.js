8c0ac0cb37c22b756028c52942e8d631cb9a868e
Mortifer <116959411+EvilMortifer@users.noreply.github.com>
2023-07-07 18:12:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,101 +1,30 @@
+$('.multiple-card-slider .carousel').each(function(){
+  var currentCarouselId = '#' + $(this).attr('id');
+  const multipleItemCarousel = document.querySelector(currentCarouselId);
 
-
-/**
- * @description Change Home page slider's arrows active status
- */
-function updateSliderArrowsStatus(
-    cardsContainer,
-    containerWidth,
-    cardCount,
-    cardWidth
-  ) {
-    if (
-      $(cardsContainer).scrollLeft() + containerWidth <
-      cardCount * cardWidth + 15
-    ) {
-      $("#slide-right-container").addClass("active");
-    } else {
-      $("#slide-right-container").removeClass("active");
-    }
-    if ($(cardsContainer).scrollLeft() > 0) {
-      $("#slide-left-container").addClass("active");
-    } else {
-      $("#slide-left-container").removeClass("active");
-    }
+  if(window.matchMedia("(min-width:576px)").matches){
+      const carousel = new bootstrap.Carousel(multipleItemCarousel, {
+          interval: false,
+          wrap: false
+      })
+      var carouselWidth = $(currentCarouselId + ' .carousel-inner')[0].scrollWidth;
+      var cardWidth = $(currentCarouselId + ' .carousel-item').width();
+      var scrollPosition = 1;    
+      $(currentCarouselId + ' .carousel-control-next').on('click', function(){
+          if(scrollPosition < (carouselWidth - (cardWidth * 2))){
+              console.log('next');
+              scrollPosition = scrollPosition + cardWidth;
+              $(currentCarouselId + ' .carousel-inner').animate({scrollLeft: scrollPosition},600);
+          }
+      });
+      $(currentCarouselId + ' .carousel-control-prev').on('click', function(){
+          if(scrollPosition > 0){
+              console.log('prev');
+              scrollPosition = scrollPosition - cardWidth;
+              $(currentCarouselId + ' .carousel-inner').animate({scrollLeft: scrollPosition},600);
+          }
+      });
+  }else{
+      $(multipleItemCarousel).addClass('slide');
   }
-  $(function() {
-    // Scroll products' slider left/right
-    let div = $("#cards-container");
-    let cardCount = $(div)
-      .find(".cards")
-      .children(".card").length;
-    let speed = 666;
-    let containerWidth = $(".container").width();
-    let cardWidth = 450;
-  
-    updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
-  
-    //Remove scrollbars
-    $("#slide-right-container").click(function(e) {
-      if ($(div).scrollLeft() + containerWidth < cardCount * cardWidth) {
-        $(div).animate(
-          {
-            scrollLeft: $(div).scrollLeft() + cardWidth
-          },
-          {
-            duration: speed,
-            complete: function() {
-              setTimeout(
-                updateSliderArrowsStatus(
-                  div,
-                  containerWidth,
-                  cardCount,
-                  cardWidth
-                ),
-                1005
-              );
-            }
-          }
-        );
-      }
-      updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
-    });
-    $("#slide-left-container").click(function(e) {
-      if ($(div).scrollLeft() + containerWidth > containerWidth) {
-        $(div).animate(
-          {
-            scrollLeft: "-=" + cardWidth
-          },
-          {
-            duration: speed,
-            complete: function() {
-              setTimeout(
-                updateSliderArrowsStatus(
-                  div,
-                  containerWidth,
-                  cardCount,
-                  cardWidth
-                ),
-                1005
-              );
-            }
-          }
-        );
-      }
-      updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
-    });
-  
-    // If resize action ocurred then update the container width value
-    $(window).resize(function() {
-      try {
-        containerWidth = $("#cards-container").width();
-        updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
-      } catch (error) {
-        console.log(
-          `Error occured while trying to get updated slider container width: 
-              ${error}`
-        );
-      }
-    });
-  });
-  
+});
